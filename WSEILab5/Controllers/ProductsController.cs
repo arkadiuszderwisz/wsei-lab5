0a7ea903f4d3dd44281cda078679687d9b4c2945
student <student@microsoft.wsei.edu.pl>
2021-10-09 14:30:17 +0200

    LAB2 do czesci AJAX

---
+++ WSEILab5/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using WSEILab5.Models;
+
+namespace WSEILab5.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductModel product)
+        {
+            var viewModel = new ProductStatsViewModel
+            {
+                NameLength = product.Name.Length,
+                DescriptionLength = product.Description.Length,
+            };
+            return View(viewModel);
+        }
+    }
+}
